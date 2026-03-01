171cbec6c29851ac9dc49e173bd7861e35b31ee4
Aurelio-0-0 <victoperez@csumb.edu>
2026-02-28 19:55:25 -0800

    Added an Exercise entity

---
+++ src/main/java/com/group9/fitnesstracker/model/Exercise.java
@@ -0,0 +1,52 @@
+package com.group9.fitnesstracker.model;
+import jakarta.annotation.Generated;
+import jakarta. persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+@Table (name = "exercises")
+public class Exercise {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(nullable = false)
+    private String muscleGroup;
+
+    public Exercise() {
+    }
+    public Exercise(String name, String description, String muscleGroup) {
+        this.name = name;
+        this.description = description;
+        this.muscleGroup = muscleGroup;
+    }
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    
+}
