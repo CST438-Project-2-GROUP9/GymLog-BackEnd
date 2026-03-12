ad86a7865308a8a70488b3797972f17870c87309
Neil Cabanilla <ncabanilla@csumb.edu>
2026-03-12 02:49:36 -0700

    Mild changes

--- src/main/java/com/group9/fitnesstracker/services/UserServiceImpl.java
+++ src/main/java/com/group9/fitnesstracker/services/UserServiceImpl.java
@@ -45,5 +45,5 @@ public class UserServiceImpl implements UserService {
     public boolean usernameExits(String username){
         return userRepository.existsByUsername(username);
     }
-
+    
 }
