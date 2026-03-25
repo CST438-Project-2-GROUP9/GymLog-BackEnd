e883afcceae9b8dc0e0eafadad4efb6c3b77bc91
Neil Cabanilla <ncabanilla@csumb.edu>
2026-03-24 20:13:27 -0700

    Fixed the issue where user is not getting authenticated even if they're an admin(Error only on render)

--- src/main/java/com/group9/fitnesstracker/config/SecurityConfig.java
+++ src/main/java/com/group9/fitnesstracker/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.group9.fitnesstracker.config;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -24,9 +26,27 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
+                        // This line basically asks if http can go and access the endpoints
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        // Does not need to be authenticated when logging in, so we can permit all
+                        .requestMatchers("/oauth2/**", "/login/**", "/error").permitAll()
+                        // Stops non admin users from going to admin routes
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+
+                // Handles the first line above if not logged in or error
+                .exceptionHandling(ex -> ex
+                        .defaultAuthenticationEntryPointFor(
+                                (request, response, authException) -> {
+                                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                                    response.setContentType("application/json");
+                                    response.getWriter().write("{\"error\":\"unauthorized\"}");
+                                },
+                                request -> request.getRequestURI().startsWith("/user/")
+                        )
+                )
+
                 // enable Google OAuth login (creates session)
                 .oauth2Login(oAuth2Login -> {
                     oAuth2Login.defaultSuccessUrl("http://localhost:5173/dashboard");
